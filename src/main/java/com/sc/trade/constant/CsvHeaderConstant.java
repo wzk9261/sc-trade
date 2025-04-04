3373012630869d0c9052995a5218f2ae6959bc4e
Jake Weng <15118126432@163.com>
2025-04-04 10:26:32 +0800

    add large_trade.csv

--- src/main/java/com/sc/trade/constant/CsvHeaderConstant.java
+++ src/main/java/com/sc/trade/constant/CsvHeaderConstant.java
@@ -7,5 +7,6 @@ public class CsvHeaderConstant {
     public static final String PRODUCT_NAME = "productName";
     public static final String CURRENCY = "currency";
     public static final String PRICE = "price";
+    public static final String[] TRADE_HEADERS = new String[]{DATE, PRODUCT_ID, CURRENCY, PRICE};
     public static final String[] ENRICHED_HEADERS = new String[]{DATE, PRODUCT_NAME, CURRENCY, PRICE};
 }
